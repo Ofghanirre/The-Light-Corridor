459df566d0d8af8f81e53c9a761ad33ecfe9347d
Ofghanirre <ofghanirre@gmail.com>
2023-04-25 10:12:02 +0200

    Added labels to vectors + compound litterals simplistic

--- headers/structs/vectors.h
+++ headers/structs/vectors.h
@@ -11,11 +11,18 @@ typedef struct Vec3D {
 
 /*------ VEC 2D ------ */
 Vec2D sum_Vec2D(Vec2D a, Vec2D b);
+#define sum_Point2D sum_Vec2D
 Vec2D mul_Vec2D(Vec2D v, float x);
+#define mul_Point2D mul_Vec2D
 int equals_Vec2D(Vec2D a, Vec2D b);
+#define equals_Point2D equals_Vec2D
 
 /*------ VEC 3D ------ */
 Vec3D sum_Vec3D(Vec3D a, Vec3D b);
+#define sum_Point3D sum_Vec3D
 Vec3D mul_Vec3D(Vec3D v, float x);
+#define mul_Point3D mul_Vec3D
 int equals_Vec3D(Vec3D a, Vec3D b);
+#define equals_Point3D equals_Vec3D
+
 #endif
--- src/structs/vectors.c
+++ src/structs/vectors.c
@@ -3,11 +3,11 @@
 
 /*------ VEC 2D ------ */
 Vec2D sum_Vec2D(Vec2D a, Vec2D b) {
-    return (Vec2D){.x = a.x + b.x, .y = a.y + b.y};
+    return (Vec2D){a.x + b.x, a.y + b.y};
 }
 
 Vec2D mul_Vec2D(Vec2D v, float x) {
-    return (Vec2D){.x = v.x * x, .y = v.y * x};
+    return (Vec2D){v.x * x, v.y * x};
 }
 
 int equals_Vec2D(Vec2D a, Vec2D b) {
@@ -17,11 +17,11 @@ int equals_Vec2D(Vec2D a, Vec2D b) {
 
 /*------ VEC 3D ------ */
 Vec3D sum_Vec3D(Vec3D a, Vec3D b) {
-    return (Vec3D){.x = a.x + b.x, .y = a.y + b.y, .z = a.z + b.z};
+    return (Vec3D){a.x + b.x, a.y + b.y, a.z + b.z};
 }
 
 Vec3D mul_Vec3D(Vec3D v, float x) {
-    return (Vec3D){.x = v.x * x, .y = v.y * x, .z = v.z * x};
+    return (Vec3D){v.x * x, v.y * x, v.z * x};
 }
 
 int equals_Vec3D(Vec3D a, Vec3D b) {
