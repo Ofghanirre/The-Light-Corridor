a3ce5b815d268d9a50f59f32c00962d6983d6f95
Ofghanirre <ofghanirre@gmail.com>
2023-04-25 10:12:09 +0200

    Added colors header

---
+++ headers/structs/colors.h
@@ -0,0 +1,12 @@
+#ifndef __COLORS_H__
+#define __COLORS_H__
+
+typedef struct ColorRGB {
+    float r, g, b;
+} ColorRGB;
+
+typedef struct ColorRGBA {
+    float r, g, b, a;
+} ColorRGBA;
+
+#endif
